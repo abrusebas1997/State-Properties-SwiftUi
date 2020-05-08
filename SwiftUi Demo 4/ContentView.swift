d8e0fb04cb0341fdb3c75205d3ff917eab3b7ad1
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-05-08 03:25:51 -0700

    heart button working

--- SwiftUi Demo 4/ContentView.swift
+++ SwiftUi Demo 4/ContentView.swift
@@ -9,6 +9,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isHearted = false
+    @State private var numHearts = 999
+    
     var body: some View {
         VStack {
             Image("foodonatewallpaper")
@@ -16,13 +19,34 @@ struct ContentView: View {
                 .cornerRadius(30)
                 .padding()
                 .shadow(radius: 10)
-            Image(systemName: "heart.fill")
+            HStack {
+                Button(action: {
+                    self.isHearted.toggle()
+                    
+                    if self.isHearted {
+                        self.numHearts += 1
+                    }
+                    else {
+                        self.numHearts -= 1
+                    }
+                    
+                }) {
+                    if isHearted{
+                        Image(systemName: "heart.fill")
+                    }
+                    else {
+                        Image(systemName: "heart")
+                    }
+                }
+                Text(String(numHearts))
+            }
             Text("Foodonate is totally amazing!")
                 .fontWeight(.heavy)
                 .foregroundColor(.blue)
                 .font(.largeTitle)
                 .multilineTextAlignment(.center)
                 .shadow(color: .black, radius: 0.1, x: 2, y: 2)
+            
         }
     }
 }
