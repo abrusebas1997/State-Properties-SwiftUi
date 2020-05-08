970aeea7f253a9bfc5da083141f0d4a3c6ca066b
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-05-08 01:30:13 -0700

    first commit

---
+++ SwiftUi Demo 4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi Demo 4
+//
+//  Created by Sebastian Abarca on 5/8/20.
+//  Copyright © 2020 Foodonate. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
